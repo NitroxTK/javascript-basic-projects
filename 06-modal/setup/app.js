98c64071a27b8f0cedbc473645f57cc4ab222653
NitroxTK <osmaracevedo13@gmail.com>
2025-11-16 11:52:28 -0500

    completed 01-06

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,25 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+
+//specifies css classes to be referenced as queries
+const modalBtn = document.querySelector('.modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+
+// add event listener for open modal
+modalBtn.addEventListener('click', function() {
+    modalOverlay.classList.add ('open-modal');
+    console.log(modalOverlay.classList); 
+
+})
+
+
+
+//add event listener for close modal
+closeBtn.addEventListener('click', function() {
+    modalOverlay.classList.remove ('open-modal');
+    console.log(modalOverlay.classList);
+})
