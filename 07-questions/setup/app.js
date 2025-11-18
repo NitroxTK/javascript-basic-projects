c5f728da04005ce9f61aa6219903aced3e45d417
NitroxTK <osmaracevedo13@gmail.com>
2025-11-18 05:46:48 -0500

    completed 07-08

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,34 @@
-//using selectors inside the element
-// traversing the dom
+// 2 methods:
+// 1) using selectors inside the element
+
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(function (question) {
+    //console.log(question);
+    const btn = question.querySelector('.question-btn');
+    console.log(btn);
+    btn.addEventListener('click', function() {
+        questions.forEach(function (item) {
+            if(item !== question) {
+                item.classList.remove("show-text");
+            }
+            console.log(item);
+        });
+
+        question.classList.toggle("show-text");
+    });
+});
+
+
+
+ 
+// 2) traversing the dom
+// const btns = document.querySelectorAll('.question-btn')
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function (e) {
+//         //console.log(e.currentTarget.parentElement.parentElement); // exact reference
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// })
--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -1,3 +1,10 @@
+// get only unique categories - HARDEST ONE
+// iterate over categories return button
+// make sure to select buttons when they are available
+
+
+
+// Items arrays
 const menu = [
   {
     id: 1,
@@ -70,5 +77,85 @@ const menu = [
     price: 16.99,
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },  
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `seered ribeye with mashed potatoes and broccoli.`,
   },
+
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container");
+
+
+// load items
+window.addEventListener('DOMContentLoaded', function () {
+  displayMenuItems(menu);
+  displayMenuBtns();
+});
+  
+
+
+
+function displayMenuItems(menuItems) {
+  // console.log("bingus bongus");
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+    return `<article class="menu-item">
+          <img src="${item.img}" class="photo" alt="${item.title}" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">${item.price}</h4>
+            </header>
+            <p class="item-text">${item.desc}</p>
+          </div>
+         </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+  //console.log(displayMenu);
+}
+
+function displayMenuBtns () {
+  const categories = menu.reduce(
+    function(values,item) {
+      if(!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+   },
+   ['all']
+   );
+   const categoryBtns = categories.map(function(category) {
+     return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`
+   }).join("");
+   container.innerHTML = categoryBtns;
+   const filterBtns = container.querySelectorAll('.filter-btn');
+   // filter items
+    filterBtns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      console.log(e.currentTarget.dataset.id);        // references html elements with attribute "data" eg <button data-id="all"></button>
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function(menuItem) {
+        // console.log(menuItem.category);
+        if(menuItem.category === category) {
+          return menuItem;
+        };
+      });
+      // console.log(menuCategory);
+      if(category === 'all') {
+        displayMenuItems(menu);
+      }
+      else{
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
+
+}
